351e4f4445147964f22013453cc90b5fd1745292
nielsdelestinne <nielsdelestinne@gmail.com>
2019-11-14 19:18:31 +0100

    initial commit

---
+++ src/main/java/be/niels/jpaskeleton/JpaSkeletonApplication.java
@@ -0,0 +1,13 @@
+package be.niels.jpaskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSkeletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSkeletonApplication.class, args);
+    }
+
+}
