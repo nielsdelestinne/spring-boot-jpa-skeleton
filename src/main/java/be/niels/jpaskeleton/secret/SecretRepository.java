215a5ff3d8942d848c0da9c504c8cad17348c6d5
nielsdelestinne <nielsdelestinne@gmail.com>
2019-11-14 20:53:16 +0100

    JpaRepository only works with Long as Id not SecretId (at least for now)

--- src/main/java/be/niels/jpaskeleton/secret/SecretRepository.java
+++ src/main/java/be/niels/jpaskeleton/secret/SecretRepository.java
@@ -1,7 +1,6 @@
 package be.niels.jpaskeleton.secret;
 
-import be.niels.jpaskeleton.secret.Secret.SecretId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SecretRepository extends JpaRepository<Secret, SecretId> {
+public interface SecretRepository extends JpaRepository<Secret, Long> {
 }
